8c8fddf40de66281046880eebe5e548fdca8d2cc
Jawad <rizvij006@gmail.com>
2025-07-24 03:50:23 -0400

    forgot to implement sheets delay variable

--- utils/sheets.py
+++ utils/sheets.py
@@ -11,12 +11,12 @@ gs_client = gspread.authorize(creds)
 sheet = gs_client.open(GOOGLE_SHEET_NAME).sheet1
 
 # If we hit the rate limit for reading, just wait
-def safe_cell_read(sheet, row, col, delay=10):
+def safe_cell_read(sheet, row, col, delay=5):
     while True:
         try:
             return sheet.cell(row, col).value
         except Exception as e:
             if "429" in str(e):
-                time.sleep(5)
+                time.sleep(delay)
             else:
                 raise
